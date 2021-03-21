a940cf17e7807a623b1d2e5db2caea3e9336b4d7
aydinacar <acaraydin01@gmail.com>
2021-03-22 01:10:43 +0300

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+import React, {useRef, useState} from "react";
+import { 
+  IonApp, 
+  IonHeader, 
+  IonContent, 
+  IonToolbar, 
+  IonTitle, 
+  IonGrid, 
+  IonRow, 
+  IonCol, 
+  IonItem, 
+  IonLabel, 
+  IonInput, 
+  IonAlert,
+} from '@ionic/react';
+
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -21,20 +33,79 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import BmiControls from "./components/BmiControls";
+import BmiResult from "./components/BmiResult";
 
-const App: React.FC = () => (
+const App: React.FC = () => {
+  const [calculateBmi, setCalculateBmi] = useState<number>();
+  const [error, setError] = useState<string>();
+  const weightInputRef = useRef<HTMLIonInputElement>(null);
+  const heightInputRef = useRef<HTMLIonInputElement>(null);
+  const calculateBMI = () => {
+    const enteredWeight = weightInputRef.current!.value;
+    const enteredHeight = heightInputRef.current!.value;
+
+    if(!enteredHeight || !enteredWeight || +enteredHeight <= 0 || +enteredWeight <= 0) {
+      setError('Please enter a valid (non-negative) input value');
+      return;
+    }
+
+    const bmi = +enteredWeight / (+enteredHeight * +enteredHeight);
+    console.log(bmi);
+    setCalculateBmi(bmi);
+  };
+  const resetInputs = () => {
+    weightInputRef.current!.value = "";
+    heightInputRef.current!.value = "";
+  };
+  const clearError = () => {
+    setError('');
+  }
+  return (
+    <React.Fragment>
+      <IonAlert isOpen={!!error} message={error} buttons={[{text: 'Okay', handler: clearError}]}></IonAlert>
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <IonHeader>
+      <IonToolbar color="primary">
+        <IonTitle>
+          BMI Calculator
+        </IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className="ion-padding">
+      <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonItem>
+              <IonLabel position="floating">
+                Your Height
+              </IonLabel>
+              <IonInput type="number" ref={heightInputRef}></IonInput>
+            </IonItem>
+          </IonCol>
+          </IonRow>
+          <IonRow>
+          <IonCol>
+          <IonItem>
+              <IonLabel position="floating">
+                Your Weight
+              </IonLabel>
+              <IonInput type="number" ref={weightInputRef}></IonInput>
+            </IonItem>
+          </IonCol>
+        </IonRow>
+        <BmiControls onCalculate={calculateBMI} onReset={resetInputs} />
+        {
+        calculateBmi ? 
+        (
+          <BmiResult result={calculateBmi} />
+        ) : null
+        }
+      </IonGrid>
+    </IonContent>
   </IonApp>
-);
+  </React.Fragment>
+  )
+};
 
 export default App;
